c1d3652f35137c9a168e05dcc2a95db19505671c
Omar Saleem <theshadowzero@gmail.com>
2017-08-29 23:03:31 -0400

    added createVideo()

--- createVideo.js
+++ createVideo.js
@@ -3,3 +3,9 @@ function printPath() {
     var imagepath = document.getElementById("imagefile").files[0].path;
     document.write("mp3:" + mp3path + "<br> image:" + imagepath);
 }
+
+function createVideo() {
+    var mp3path = document.getElementById("mp3file").files[0].path;
+    var imagepath = document.getElementById("imagefile").files[0].path;
+    // call ffmpeg here?
+}
