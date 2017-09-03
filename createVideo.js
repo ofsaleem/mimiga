14681c657ec47228b5c328be439c0419c6f6c548
Omar Saleem <theshadowzero@gmail.com>
2017-09-03 05:35:56 -0400

    added option to pick visualizer

--- createVideo.js
+++ createVideo.js
@@ -1,6 +1,7 @@
 var imagewidth = 600;
 var imageheight = 600;
 var fixedimagewidth = 600;
+var ffmpeg = require('fluent-ffmpeg');
 
 function getImageWidth(input) {
     var fr = new FileReader;
@@ -19,246 +20,126 @@ function getImageWidth(input) {
     fixedimagewidth = fixedimagewidth.toFixed(0);
 }
 
-
-function createVideo() {
-    var ffmpeg = require('fluent-ffmpeg');
-    var mp3path = document.getElementById("mp3file").files[0].path;
-    var imagepath = document.getElementById("imagefile").files[0].path;
-    var output = document.getElementById('output');
+function renderWithWaveforms(mp3path, imagepath, output) {
     ffmpeg().input(mp3path).input(imagepath)
     .inputOptions(['-loop 1'])
+    .complexFilter([
+        {
+            filter: 'color',
+            options: {
+                s: '1920x1080',
+                c: 'black'
+            },
+            outputs: ['blackbg']
+        },
+        {
+            filter: 'showwaves',
+            options: {
+                s: fixedimagewidth + 'x400',
+                rate: '24',
+                colors: 'cyan|gray',
+                mode: 'line',
+                scale: 'sqrt'
+            },
+            inputs: ['0:a'],
+            outputs: ['wave']
+        },
+        {
+            filter: 'split',
+            inputs: ['wave'],
+            outputs: ['wave1', 'wave2']
+        },
+        {
+            filter: 'scale',
+            options: {
+                w: '-1',
+                h: '600',
+            },
+            inputs: ['1:v'],
+            outputs: ['pic']
+        },
+        {
+            filter: 'overlay',
+            options: {
+                x: '(W-w)/2',
+                y: '40',
+                eval: 'init'
+            },
+            inputs: ['blackbg', 'wave1'],
+            outputs: ['waves']
+        },
+        {
+            filter: 'overlay',
+            options: {
+                x: '(W-w)/2',
+                y: '640',
+                eval: 'init'
+            },
+            inputs: ['waves', 'wave2'],
+            outputs: ['bg']
+        },
+        {
+            filter: 'boxblur',
+            options: [
+                '20',
+                '2'
+            ],
+            inputs: ['bg'],
+            outputs: ['bg']
+        },
+        {
+            filter: 'overlay',
+            options: {
+                x: '(W-w)/2',
+                y: '(H-h)/2',
+                eval: 'init'
+            },
+            inputs: ['bg', 'pic'],
+            outputs: ['out']
+        }
+    ], 'out')
+    .outputOption(['-map 0:a'])
+    .videoCodec('libx264')
+    .outputOption(['-crf 20'])
+    .audioCodec('aac')
+    .audioBitrate(320)
+    .outputOption(['-pix_fmt yuv420p'])
+    .fpsOutput(24)
+    .outputOption(['-preset slow'])
+    .outputOption(['-movflags +faststart'])
+    .outputOption(['-profile:v high'])
+    .outputOption(['-level 4.0'])
+    .outputOption(['-bf 2'])
+    .outputOption(['-g 12'])
+    .outputOption(['-coder 1'])
+    .audioChannels(2)
+    .audioFrequency(48000)
+    .outputOption(['-shortest'])
+    .on('start', function(commandLine) {
+        output.innerHTML += 'Spawned Ffmpeg with command: ' + commandLine + '\n' +
+        'Beginning encoding\n';
+        output.scrollTop = output.scrollHeight - output.clientHeight;
+    })
+    .on('progress', function(progress) {
+        var text = output.innerHTML;
+        output.innerHTML = text.replace(/\r?\n?[^\r\n]*$/, "");
+        output.innerHTML += '\nProcessing: ' + progress.percent + '% done';
+        output.scrollTop = output.scrollHeight - output.clientHeight;
+    })
+    .on('error', function(err) {
+        output.innerHTML += '\nAn error occurred: ' + err.message + '\n';
+        output.scrollTop = output.scrollHeight - output.clientHeight;
+    })
+    .on('end', function() {
+        output.innerHTML += '\nOutput finished!\n';
+        output.scrollTop = output.scrollHeight - output.clientHeight;
+    })
+    .save('output.mp4');
+}
 
-    // line avectorscope
-    // .complexFilter([
-    //     {
-    //         filter: 'avectorscope',
-    //         options: {
-    //             s: '1920x1080',
-    //             rate: '24',
-    //             zoom: '6',
-    //             draw: 'line',
-    //         },
-    //         inputs: '0:a',
-    //         outputs: 'outa'
-    //     },
-    //     {
-    //         filter: 'scale',
-    //         options: {
-    //             w: '800',
-    //             h: '600',
-    //             force_original_aspect_ratio: 'decrease'
-    //         },
-    //         inputs: '1:v',
-    //         outputs: 'outv'
-    //     },
-    //     {
-    //         filter: 'overlay',
-    //         options: {
-    //             x: '(W-w)/2',
-    //             y: '(H-h)/2'
-    //         },
-    //         inputs: ['outa', 'outv'],
-    //         outputs: 'out'
-    //     },
-    //     {
-    //         filter: 'scale',
-    //         options: {
-    //             w: '1920',
-    //             h: '1080'
-    //         },
-    //         inputs: 'out',
-    //         outputs: 'out'
-    //     }
-    //
-    // ], 'out')
-
-    // all 4 sides waveforms
-    // .complexFilter([
-    //     {
-    //         filter: 'color',
-    //         options: {
-    //             s: '1920x1080',
-    //             c: 'black'
-    //         },
-    //         outputs: ['blackbg']
-    //     },
-    //     {
-    //         filter: 'showwaves',
-    //         options: {
-    //             s: fixedimagewidth + 'x400',
-    //             rate: '24',
-    //             colors: 'cyan|gray',
-    //             mode: 'line',
-    //             scale: 'lin'
-    //         },
-    //         inputs: ['0:a'],
-    //         outputs: ['wave']
-    //     },
-    //     {
-    //         filter: 'split',
-    //         options: '4',
-    //         inputs: ['wave'],
-    //         outputs: ['wave1', 'wave2', 'wave3', 'wave4']
-    //     },
-    //     {
-    //         filter: 'rotate',
-    //         options: {
-    //             a: 'PI/2',
-    //             ow: '400',
-    //             oh: fixedimagewidth
-    //         },
-    //         inputs: ['wave3'],
-    //         outputs: ['wave3']
-    //     },
-    //     {
-    //         filter: 'rotate',
-    //         options: {
-    //             a: '-PI/2',
-    //             ow: '400',
-    //             oh: fixedimagewidth
-    //         },
-    //         inputs: ['wave4'],
-    //         outputs: ['wave4']
-    //     },
-    //     {
-    //         filter: 'scale',
-    //         options: {
-    //             w: '-1',
-    //             h: '600',
-    //         },
-    //         inputs: ['1:v'],
-    //         outputs: ['pic']
-    //     },
-    //     {
-    //         filter: 'overlay',
-    //         options: {
-    //             x: '(W-w)/2',
-    //             y: '40',
-    //             eval: 'init'
-    //         },
-    //         inputs: ['blackbg', 'wave1'],
-    //         outputs: ['bg']
-    //     },
-    //     {
-    //         filter: 'overlay',
-    //         options: {
-    //             x: '(W-w)/2',
-    //             y: '640',
-    //             eval: 'init'
-    //         },
-    //         inputs: ['bg', 'wave2'],
-    //         outputs: ['bg']
-    //     },
-    //     {
-    //         filter: 'overlay',
-    //         options: {
-    //             x: '1060',
-    //             y: '(H-h)/2',
-    //             eval: 'init'
-    //         },
-    //         inputs: ['bg', 'wave3'],
-    //         outputs: ['bg']
-    //     },
-    //     {
-    //         filter: 'overlay',
-    //         options: {
-    //             x: '460',
-    //             y: '(H-h)/2',
-    //             eval: 'init'
-    //         },
-    //         inputs: ['bg', 'wave4'],
-    //         outputs: ['bg']
-    //     },
-    //     {
-    //         filter: 'overlay',
-    //         options: {
-    //             x: '(W-w)/2',
-    //             y: '(H-h)/2',
-    //             eval: 'init'
-    //         },
-    //         inputs: ['bg', 'pic'],
-    //         outputs: ['out']
-    //     }
-    // ], 'out') with blur
-
-    // top bottom waveforms with blur
-    // .complexFilter([
-    //     {
-    //         filter: 'color',
-    //         options: {
-    //             s: '1920x1080',
-    //             c: 'black'
-    //         },
-    //         outputs: ['blackbg']
-    //     },
-    //     {
-    //         filter: 'showwaves',
-    //         options: {
-    //             s: fixedimagewidth + 'x400',
-    //             rate: '12',
-    //             colors: 'cyan|gray',
-    //             mode: 'line',
-    //             scale: 'lin'
-    //         },
-    //         inputs: ['0:a'],
-    //         outputs: ['wave']
-    //     },
-    //     {
-    //         filter: 'split',
-    //         inputs: ['wave'],
-    //         outputs: ['wave1', 'wave2']
-    //     },
-    //     {
-    //         filter: 'scale',
-    //         options: {
-    //             w: '-1',
-    //             h: '600',
-    //         },
-    //         inputs: ['1:v'],
-    //         outputs: ['pic']
-    //     },
-    //     {
-    //         filter: 'overlay',
-    //         options: {
-    //             x: '(W-w)/2',
-    //             y: '40',
-    //             eval: 'init'
-    //         },
-    //         inputs: ['blackbg', 'wave1'],
-    //         outputs: ['waves']
-    //     },
-    //     {
-    //         filter: 'overlay',
-    //         options: {
-    //             x: '(W-w)/2',
-    //             y: '640',
-    //             eval: 'init'
-    //         },
-    //         inputs: ['waves', 'wave2'],
-    //         outputs: ['bg']
-    //     },
-    //     {
-    //         filter: 'boxblur',
-    //         options: [
-    //             '20',
-    //             '2'
-    //         ],
-    //         inputs: ['bg'],
-    //         outputs: ['bg']
-    //     },
-    //     {
-    //         filter: 'overlay',
-    //         options: {
-    //             x: '(W-w)/2',
-    //             y: '(H-h)/2',
-    //             eval: 'init'
-    //         },
-    //         inputs: ['bg', 'pic'],
-    //         outputs: ['out']
-    //     }
-    // ], 'out')
-
-    // avectorscope with blur
+function renderWithVectorscope(mp3path, imagepath, output) {
+    ffmpeg().input(mp3path).input(imagepath)
+    .inputOptions(['-loop 1'])
     .complexFilter([
         {
             filter: 'avectorscope',
@@ -337,6 +218,19 @@ function createVideo() {
         output.scrollTop = output.scrollHeight - output.clientHeight;
     })
     .save('output.mp4');
+}
+
+function createVideo() {
+    var mp3path = document.getElementById("mp3file").files[0].path;
+    var imagepath = document.getElementById("imagefile").files[0].path;
+    var output = document.getElementById('output');
+    var visualizer = document.getElementById('visualizer');
+    if (visualizer.value == 'vectorscope') {
+        renderWithVectorscope(mp3path, imagepath, output);
+    }
+    else {
+        renderWithWaveforms(mp3path, imagepath, output);
+    }
 }
 
 function getFfmpeg() {
