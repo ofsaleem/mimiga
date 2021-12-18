c2ca683c451d66b79447068042e2238fa2dfab70
Omar Saleem <ofsaleem@users.noreply.github.com>
2021-12-18 01:31:04 -0500

    add semi-colons and give preload absolute path

--- main.js
+++ main.js
@@ -5,38 +5,39 @@ const createWindow = () => {
   const win = new BrowserWindow({
       width: 800,
       height: 600,
-      preload: 'uploadVideo.js',
-      preload: 'createVideo.js'
-  })
+      webPreferences: {
+        preload: `${__dirname}/preload.js`,
+      }
+  });
 
   // and load the index.html of the app.
   win.loadFile('index.html');
 
   // Open the DevTools.
-  //win.webContents.openDevTools()
+  win.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
