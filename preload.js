f9df670b87629d8347145fa3699d681e6948b551
Omar Saleem <ofsaleem@users.noreply.github.com>
2021-12-18 04:17:27 -0500

    removing remote requires from preload

--- preload.js
+++ preload.js
@@ -1,6 +1,2 @@
 const { shell } = require('electron');
-const google = require('googleapis');
-const Lien = require('lien');
-const util = require('util');
-const fs = require("fs");
-const ffmpeg = require('fluent-ffmpeg');
+
