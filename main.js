3da62fa01f50394146905273a60dd20ad7bcb600
Omar Saleem <ofsaleem@users.noreply.github.com>
2021-12-18 01:03:33 -0500

    refactoring main to be compliant with modern electron standards

--- main.js
+++ main.js
@@ -1,35 +1,19 @@
 const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win
-
-function createWindow () {
+const createWindow = () => {
   // Create the browser window.
-  win = new BrowserWindow({
+  const win = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 600,
+      preload: 'uploadVideo.js',
+      preload: 'createVideo.js'
   })
 
   // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  win.loadFile('index.html');
 
   // Open the DevTools.
   //win.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
 }
 
 // This method will be called when Electron has finished
