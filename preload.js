32f98e31e24250df446c69f2d1324806d1692f59
Omar Saleem <ofsaleem@users.noreply.github.com>
2021-12-18 01:31:18 -0500

    create preload.js with all the requires i need

---
+++ preload.js
@@ -0,0 +1,6 @@
+const { shell } = require('electron');
+const google = require('googleapis');
+const Lien = require('lien');
+const util = require('util');
+const fs = require("fs");
+const ffmpeg = require('fluent-ffmpeg');
