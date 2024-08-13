f2d2147cd209ae4446fc970ef18279980524a607
Omar Saleem <ofsaleem@users.noreply.github.com>
2024-08-13 16:50:40 -0400

    moving createVideo back out

--- main.js
+++ main.js
@@ -221,16 +221,3 @@ ipcMain.handle('ffmpeg-waveforms', async (event, mp3path, imagepath, fixedImageW
   })
   .save('output.mp4');
 });
-
-function createVideo() {
-    let mp3path = document.getElementById("mp3file").files[0].path;
-    let imagepath = document.getElementById("imagefile").files[0].path;
-    let visualizer = document.getElementById('visualizer');
-    if (visualizer.value == 'vectorscope') {
-        ffmpeg.renderWithVectorscope(mp3path, imagepath);
-    }
-    else {
-        let fixedImageWidth = getImageWidth(document.getElementById("imagefile"));
-        ffmpeg.renderWithWaveforms(mp3path, imagepath, fixedImageWidth);
-    }
-}
