a251a6126bacff912d057ee3c641b720cf1a5ff3
Omar Saleem <ofsaleem@users.noreply.github.com>
2024-08-13 17:26:59 -0400

    made download ffmpeg button work

--- renderer.js
+++ renderer.js
@@ -1,10 +1,8 @@
-const { ipcRenderer } = require("electron");
-
-
-function createVideo() {
-    let mp3path = document.getElementById("mp3file").files[0].path;
-    let imagepath = document.getElementById("imagefile").files[0].path;
-    let visualizer = document.getElementById('visualizer');
+const createBtn = document.getElementById('create-video');
+createBtn.addEventListener('click', () => {
+    const mp3path = document.getElementById("mp3file").files[0].path;
+    const imagepath = document.getElementById("imagefile").files[0].path;
+    const visualizer = document.getElementById('visualizer');
     if (visualizer.value == 'vectorscope') {
         ffmpeg.renderWithVectorscope(mp3path, imagepath);
     }
@@ -12,4 +10,14 @@ function createVideo() {
         let fixedImageWidth = getImageWidth(document.getElementById("imagefile"));
         ffmpeg.renderWithWaveforms(mp3path, imagepath, fixedImageWidth);
     }
-}
+})
+
+const getFfmpegBtn = document.getElementById('get-Ffmpeg');
+getFfmpegBtn.addEventListener('click', async () => {
+    let platform = await window.ffbinaries.downloadFFBinaries();
+    output.innerHTML += '\nDownloaded ffmpeg and ffprobe for platform ' + platform;
+    output.scrollTop = output.scrollHeight - output.clientHeight;
+})
+
+const authBtn = document.getElementById('youtube-auth');
+const uploadBtn = document.getElementById('upload-video');
