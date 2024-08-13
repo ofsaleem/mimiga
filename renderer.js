70e17d93408c7e7a060e577f80def7be8df95152
Omar Saleem <ofsaleem@users.noreply.github.com>
2024-08-13 17:33:31 -0400

    moved getImageWidth into renderer.js

--- renderer.js
+++ renderer.js
@@ -1,14 +1,33 @@
+function getImageWidth(input) {
+    let fixedImageWidth = 600;
+    let fr = new FileReader;
+    fr.onload = () => {
+        let img = new Image;
+        img.onload = () => {
+            let imagewidth = +img.width;
+            let imageheight = +img.height;
+            let ar = imagewidth * 1.0 / imageheight;
+            ar = +(parseFloat(ar.toFixed(2)));
+            fixedImageWidth = 600 * ar;
+            fixedImageWidth = +fixedImageWidth.toFixed(0);
+        };
+        img.src = fr.result;
+    };
+    fr.readAsDataURL(input.files[0]);
+    return fixedImageWidth;
+}
+
 const createBtn = document.getElementById('create-video');
 createBtn.addEventListener('click', () => {
     const mp3path = document.getElementById("mp3file").files[0].path;
     const imagepath = document.getElementById("imagefile").files[0].path;
     const visualizer = document.getElementById('visualizer');
     if (visualizer.value == 'vectorscope') {
-        ffmpeg.renderWithVectorscope(mp3path, imagepath);
+        window.ffmpeg.renderWithVectorscope(mp3path, imagepath);
     }
     else {
         let fixedImageWidth = getImageWidth(document.getElementById("imagefile"));
-        ffmpeg.renderWithWaveforms(mp3path, imagepath, fixedImageWidth);
+        window.ffmpeg.renderWithWaveforms(mp3path, imagepath, fixedImageWidth);
     }
 })
 
